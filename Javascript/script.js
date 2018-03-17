0150d0e75b055fd97598e0eb35159a439cda622c
Serghi0 <s_l_l_@msn.com>
2018-03-17 04:57:12 +0100

    dynamic background fix

--- Javascript/script.js
+++ Javascript/script.js
@@ -2,14 +2,34 @@
 		tl = new TimelineMax({paused: true}),
 		dot = document.getElementsByClassName('dot'),
 		loader = document.getElementById('loader'),
-		main = document.getElementsByTagName('main');
+		main = document.getElementsByTagName('main'),
+		header = document.getElementsByTagName('header')
+		footer = document.getElementsByTagName('footer'),
+		p0 = document.getElementById('p0'),
+		p1 = document.getElementById('p1'),
+		p2 = document.getElementById('p2'),
+		p3 = document.getElementById('p3'),
+		p4 = document.getElementById('p4'),
+		p5 = document.getElementById('p5'),
+		p6 = document.getElementById('p6');
+
 		tlLoader = new TimelineMax({repeat:2, onComplete: loadContent});
 
 //Timeline
 	tl
 		.set(main, {autoAlpha:1})
-		.from(clock, 1, {autoAlpha:0, x:-500, y:-100, ease:Back.easeNone, fontSize:1}) //ease:Power0.easeNone, easeOut, easeIn, Power1 or 2 or 3 or 4
-		.to(clock,2, {fontSize:40, ease:Power4.easeOut}, '-=0.15'); //3,2,1 absolute position
+		.fromTo(header,1,{autoAlpha:0, y:-100}, {autoAlpha:1, y:0})
+		.fromTo(footer,1,{autoAlpha:0, y:100}, {autoAlpha:1, y:0}, '-=1')
+		/*.fromTo(p0,1,{autoAlpha:0, y:100}, {autoAlpha:1, y:0}, '-=1')
+		.fromTo(p1,1,{autoAlpha:0, y:100}, {autoAlpha:1, y:0}, '-=1')
+		.fromTo(p2,1,{autoAlpha:0, y:100}, {autoAlpha:1, y:0}, '-=1')
+		.fromTo(p3,1,{autoAlpha:0, y:100}, {autoAlpha:1, y:0}, '-=1')
+		.fromTo(p4,1,{autoAlpha:0, y:100}, {autoAlpha:1, y:0}, '-=1')
+		.fromTo(p5,1,{autoAlpha:0, y:100}, {autoAlpha:1, y:0}, '-=1')
+		.fromTo(p6,1,{autoAlpha:0, y:100}, {autoAlpha:1, y:0}, '-=1')*/
+		.from(clock, 1, {autoAlpha:0, x:-500, y:-100, ease:Back.easeNone, fontSize:0}) //ease:Power0.easeNone, easeOut, easeIn, Power1 or 2 or 3 or 4
+		.to(clock,1, {fontSize:40, ease:Power4.easeOut}, '-=0.15');
+		 //3,2,1 absolute position
 
 //Loader Timeline
 	tlLoader
@@ -27,8 +47,8 @@
 function loadContent(){
 var tlLoaderOut = new TimelineMax({onComplete: contentIn});
 	tlLoaderOut
-			.set(dot,{backgroundColor:'#000000'})
-			.to(loader, 0.3, {autoAlpha:1, scale:1.3, ease:Power0.easeNone})
+			.set(dot,{backgroundColor:'#222222'})
+			.to(loader, 0.3, {autoAlpha:1, scale:1, ease:Power0.easeNone})
 			.staggerFromTo(dot, 0.3,
 						{y: 0, autoAlpha:0},
 						{y: 20, autoAlpha: 1, ease:Back.easeInOut},
@@ -62,36 +82,60 @@ function initiateClock() {
 function getTime() {
 	var d = new Date();
 	var time = d.toLocaleTimeString();
-	//var hours = d.getHours();
 	document.getElementById('clock').innerHTML = time;
-	/*if(hours==10){
-		console.log('yes');
-	}else{
-		console.log('lol');
-	}*/
 	return d;
 }
 
 function dynamicBackground(){
-/*	if(getTime().hours==10){
-		console.log('yes');
+	var hour = getTime().getHours(),
+		minutes = getTime().getMinutes(),
+		backgroundTl = new TimelineMax(),
+		body = document.getElementsByTagName('body');
+	console.log(hour + ':' + minutes);
+	
+
+
+
+	if(hour<5  || hour>19 ){
+ 	backgroundTl.to(body,1,{backgroundColor:"#001848"});
+		console.log('night');
+	}else if(hour==5){
+		if(minutes<30){
+ 			backgroundTl.to(body,1,{backgroundColor:"#001848"});
+			console.log('night');
+		}else{
+		 	backgroundTl.to(body,1,{backgroundColor:"#fec5b9"});
+			console.log('dawn');
+		}
+	}else if((hours>6 && hours<7)||(hours>18 &&hours<19)){
+		 	backgroundTl.to(body,1,{backgroundColor:"#fec5b9"});
+			console.log('dusk/dawn');		
+	}else if(hours==17){
+		if(minutes<30){
+		 	backgroundTl.to(body,1,{backgroundColor:"#87ceeb"});
+			console.log('day');
+		}else{
+		 	backgroundTl.to(body,1,{backgroundColor:"#fec5b9"});
+			console.log('dusk');
+		}
 	}else{
-		console.log('lol');
+		 	backgroundTl.to(body,1,{backgroundColor:"#87ceeb"});
+		console.log('day');
 	}
-*/
-var hour = getTime().getHours();
-var minutes = getTime().getMinutes();
-//console.log(hour + ' ' + minutes);
+	
+/*
 	switch(true){
-		case(hour<7):
+		case((hour<5 && minutes>30) || hour>19):
+		//document.getElementById('clock').style.color ='white';
+		document.body.style.backgroundColor = "#001848";
 		console.log('lol');
 		break;
-		case(hour>=10 && minutes>50):
+		case(hour>=7 && hour<=17):
 		console.log('yay');
 		break;
 		default:
 		console.log('ok');
-	}
+	}*/
 }
 
 initiateClock();
