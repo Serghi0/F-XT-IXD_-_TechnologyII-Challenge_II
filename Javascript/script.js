5d8b4338d46b6dc358b0817bdc0b26ebb09cb38f
Serghi0 <s_l_l_@msn.com>
2018-03-18 19:22:53 +0100

    fixed code

--- Javascript/script.js
+++ Javascript/script.js
@@ -21,7 +21,7 @@
 		.fromTo(clouds,1,{autoAlpha:0, y:100}, {autoAlpha:1, y:0}, '-=1')
 		.from(clock, 1, {autoAlpha:0, x:-500, y:-100, ease:Back.easeNone, fontSize:0}) //ease:Power0.easeNone, easeOut, easeIn, Power1 or 2 or 3 or 4
 		.to(clock,1, {autoAlpha:1, fontSize:32, ease:Power4.easeOut}, '-=0.15') //3,2,1 absolute position		
-		.fromTo(rocket,4,{autoAlpha:0, x:-1000, y:1000}, {autoAlpha:1, x:0, y:0})
+		.fromTo(rocket,8,{autoAlpha:0, x:-1000, y:1000}, {autoAlpha:1, x:0, y:0})
 		.fromTo(rocket,0.01,{x:-0.75}, {x:0.75, clearProps:"x", repeat:-1});
 
 //Loader Timeline
@@ -88,6 +88,14 @@ function dynamicBackground(){
 	if(hour<5  || hour>19 ){
  	backgroundTl.to(body,1,{backgroundColor:"#001848"});
 		//console.log('night');
+	}else if(hour==19){
+		if(minutes<30){
+		 	backgroundTl.to(body,1,{backgroundColor:"#fec5b9"});
+			//console.log('dusk');
+		}else{
+ 	backgroundTl.to(body,1,{backgroundColor:"#001848"});
+		//console.log('night');
+		}
 	}else if(hour==5){
 		if(minutes<30){
  			backgroundTl.to(body,1,{backgroundColor:"#001848"});
@@ -96,9 +104,17 @@ function dynamicBackground(){
 		 	backgroundTl.to(body,1,{backgroundColor:"#fec5b9"});
 			//console.log('dawn');
 		}
-	}else if((hour>6 && hour<7)||(hour>18 &&hour<19)){
+	}else if((hour>=6 && hour<=7)||(hour>=18 &&hour<19)){
 		 	backgroundTl.to(body,1,{backgroundColor:"#fec5b9"});
 			//console.log('dusk/dawn');		
+	}else if(hour==7){
+		if(minutes<30){
+		 	backgroundTl.to(body,1,{backgroundColor:"#fec5b9"});
+			//console.log('dawn');
+		}else{
+		 	backgroundTl.to(body,1,{backgroundColor:"#87ceeb"});
+			//console.log('day');
+		}
 	}else if(hour==17){
 		if(minutes<30){
 		 	backgroundTl.to(body,1,{backgroundColor:"#87ceeb"});
